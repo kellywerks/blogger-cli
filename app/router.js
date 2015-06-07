3c8aed179d42dee8f72617bcdd0e30baf986bd83
Kelly Becker <kellywerks@gmail.com>
2015-06-07 13:36:58 -0700

    add user model

--- app/router.js
+++ app/router.js
@@ -6,7 +6,12 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  //route vs resource - route is static pages, resource is model-backed, dynamic
+  //resource is a 'thing', route is something to do with a 'thing'
   this.resource('posts', { path: '/'});
+  this.resource('post', { path: '/posts/:post_id'});
+  //when prefaced by a colon, it is a 'path segment' which is a dynamic element
+  //tells ember 'go find a post with a post_id that is present in the path segment'
   this.route('about');
   this.resource('recent-comments');
   this.resource('contact', function() {
